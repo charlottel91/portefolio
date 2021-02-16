ac2bfaddc6c922e10f3342953a116bc5e5141eb9
charlottel91 <charlotte.leconte91@gmail.com>
2021-02-16 14:43:08 +0100

    Add assets.

---
+++ src/assets/index.js
@@ -0,0 +1,7 @@
+import Introduce from './introduce/Intruduce'
+import Navbar from './navbar/Navbar'
+import Pdf from './pdf/Pdf'
+import Project from './projects/Project'
+import Resume from './resume/Resume'
+
+export { Introduce, Navbar, Pdf, Project, Resume }
